9c156f06214554ee935b1b70efbf36e325bd0ad9
coralia <marvinparrales2021@gmial.com>
2022-03-03 15:59:24 -0600

    Bumer

---
+++ src/javaapplication10/JavaApplication10.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication10;
+
+/**
+ *
+ * @author marvi
+ */
+public class JavaApplication10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int contador = 2, numero=0;
+        boolean primo=true;
+        
+        while ((primo) && (contador!=numero)){
+            if (numero % contador == 0)
+                primo = false;
+                contador++;
+            }
+        
+        System.out.println(contador);
+        System.out.println(contador);
+        System.out.println(contador);
+        System.out.println(contador);
+    }
+    
+}
